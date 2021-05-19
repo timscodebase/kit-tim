1e058af3169c3910fc04cfa0410880d4bfb2eee1
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2021-05-18 19:51:03 -0700

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-const netlify = require('@sveltejs/adapter-netlify')
+import netlify from '@sveltejs/adapter-netlify'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,6 +6,11 @@ const config = {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
 		adapter: netlify(),
+		vite: {
+			ssr: {
+				noExternal: Object.keys(pkg.dependencies || {}),
+			},
+		},
 	}
 };
 
